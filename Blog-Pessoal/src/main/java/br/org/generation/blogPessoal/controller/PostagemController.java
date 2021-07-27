2b6365fb6c67706cd72aa04ffd8d0fe21645d4f7
Yoha lima <86079883+Yehokhananlima@users.noreply.github.com>
2021-07-26 22:09:49 -0300

    Blog pessoal

---
+++ Blog-Pessoal/src/main/java/br/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,18 @@
+package br.org.generation.blogPessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+
+}
